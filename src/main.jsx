1544a4d85827bb4796628ee2c1036eb84a19247f
BobBieNeno <bie007x@gmail.com>
2026-06-12 15:57:26 +0700

    fix frontend build dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './i18n/index.js'   // ← i18next init ที่นี่
-import App from './App'
-import { CartProvider } from './context/CartContext'
+import App from './App.jsx'
+import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
